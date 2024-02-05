3c45722da745411daf826fe0ac38e25cc634a014
Xinyisun-creator <am1181094764@gmail.com>
2024-02-05 10:02:55 +0000

    ELEGANTO CODE

--- section_D_Q2.m
+++ section_D_Q2.m
@@ -3,7 +3,6 @@ col = 'rbcmy';
 
 %%% data processing
 
-% 初始化空矩阵来存储训练数据和测试数据
 train_data = [];
 test_data = [];
 train_labels = [];
@@ -15,27 +14,25 @@ for i = 1:6
 end
 
 for i = 1:10:size(projection3D, 1)
-    % 确保不会超出矩阵的边界
     end_idx = min(i+9, size(projection3D, 1));
     batch = projection3D(i:end_idx, :);
-    batch_labels = labels(i:end_idx); % 取对应批次的标签
+    batch_labels = labels(i:end_idx); 
     num_rows = size(batch, 1);
     if num_rows == 10
         train_data = [train_data; batch(1:6, :)];
         test_data = [test_data; batch(7:10, :)];
-        train_labels = [train_labels; batch_labels(1:6)]; % 分配训练标签
-        test_labels = [test_labels; batch_labels(7:10)]; % 分配测试标签
+        train_labels = [train_labels; batch_labels(1:6)]; 
+        test_labels = [test_labels; batch_labels(7:10)]; 
     else
-        % 对于不足10行的最后一批数据，按比例分割
         split_point = ceil(num_rows*0.6);
         train_data = [train_data; batch(1:split_point, :)];
         test_data = [test_data; batch(split_point+1:end, :)];
-        train_labels = [train_labels; batch_labels(1:split_point)]; % 分配训练标签
-        test_labels = [test_labels; batch_labels(split_point+1:end)]; % 分配测试标签
+        train_labels = [train_labels; batch_labels(1:split_point)]; 
+        test_labels = [test_labels; batch_labels(split_point+1:end)]; 
     end
 end
 
-accuracy = []
+accuracy = [];
 
 for i=1:length(leaf)
     baggedModel = TreeBagger(leaf(i), train_data,train_labels , 'OOBPredictorImportance', 'On');
@@ -46,6 +43,7 @@ for i=1:length(leaf)
     % c. Run the trained model with the test data and display a confusion matrix
     [Y_pred, scores] = predict(baggedModel, test_data);
     confMat = confusionmat(test_labels, str2double(Y_pred));
+    figure; 
     confusionchart(confMat);
     xlabel('Predicted Class');
     ylabel('True Class');
